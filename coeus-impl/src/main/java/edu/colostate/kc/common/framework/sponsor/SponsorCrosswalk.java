d55223233050b7e7454dfb477556c572c14ec39d
Tyler Wilson <tyler.wilson@colostate.edu>
2016-10-20 00:22:29 -0600

    Updated SponsorCrosswalk

--- coeus-impl/src/main/java/edu/colostate/kc/common/framework/sponsor/SponsorCrosswalk.java
+++ coeus-impl/src/main/java/edu/colostate/kc/common/framework/sponsor/SponsorCrosswalk.java
@@ -1,6 +1,6 @@
 package edu.colostate.kc.common.framework.sponsor;
 
-import com.sun.org.apache.xpath.internal.operations.String;
+import java.lang.String;
 import edu.colostate.kc.common.api.sponsor.SponsorCrosswalkContract;
 import org.kuali.coeus.common.framework.sponsor.Sponsor;
 import org.kuali.coeus.sys.framework.model.KcPersistableBusinessObjectBase;
@@ -9,25 +9,41 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "CSU_SPONSOR_CROSSWALK")
-public class SponsorCrosswalk extends KcPersistableBusinessObjectBase implements SponsorCrosswalkContract {
+public class SponsorCrosswalk extends KcPersistableBusinessObjectBase {
 
     @Id
+    @Column(name = "SPONSOR_CODE")
+    private String sponsorCode;
+
     @Column(name = "SPONSOR_ID")
     private String sponsorId;
 
-    @Transient
-    private String sponsorCode;
+    @OneToOne(cascade = { CascadeType.REFRESH })
+    @JoinColumn(name = "SPONSOR_CODE", referencedColumnName = "SPONSOR_CODE", insertable = false, updatable = false)
+    private Sponsor sponsor;
 
-    @Override
     public String getSponsorId() { return sponsorId; }
     public void setSponsorId(String sponsorId) { this.sponsorId = sponsorId; }
 
-    @Override
     public String getSponsorCode() { return sponsorCode; }
     public void setSponsorCode(String sponsorCode) { this.sponsorCode = sponsorCode;}
 
-    @ManyToOne(cascade = { CascadeType.REFRESH })
-    @JoinColumn(name = "SPONSOR_CODE", referencedColumnName = "SPONSOR_CODE", insertable = false, updatable = false)
-    private Sponsor sponsor;
+    /**
+     * Sponsor reference referred by {@link #getSponsorCode()}
+     *
+     * @param sponsor
+     */
+    public void setSponsor(Sponsor sponsor) {
+        this.sponsor = sponsor;
+    }
+
+    /**
+     * Sponsor reference referred by {@link #getSponsorCode()}
+     *
+     * @return Sponsor
+     */
+    public Sponsor getSponsor() {
+        return sponsor;
+    }
 
 }
