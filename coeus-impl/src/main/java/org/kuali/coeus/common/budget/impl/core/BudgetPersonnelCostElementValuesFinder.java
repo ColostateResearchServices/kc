a4882de102749c7657c34486f07f68399d48a01b
Chris Denne <chris.denne@colostate.edu>
2017-01-04 15:42:41 -0700

    RSISSUES-148: Added logic to limit Obj Code to valid choice for selected person's jobcode

--- coeus-impl/src/main/java/org/kuali/coeus/common/budget/impl/core/BudgetPersonnelCostElementValuesFinder.java
+++ coeus-impl/src/main/java/org/kuali/coeus/common/budget/impl/core/BudgetPersonnelCostElementValuesFinder.java
@@ -18,12 +18,20 @@
  */
 package org.kuali.coeus.common.budget.impl.core;
 
-import java.util.List;
+import java.util.*;
 
+import org.apache.commons.lang3.StringUtils;
+import org.kuali.coeus.common.budget.framework.personnel.BudgetPersonnelDetails;
+import org.kuali.coeus.common.budget.framework.personnel.ValidCeJobCode;
 import org.kuali.kra.infrastructure.Constants;
 import org.kuali.rice.core.api.util.KeyValue;
 import org.kuali.rice.coreservice.framework.parameter.ParameterConstants;
 import org.kuali.rice.coreservice.framework.parameter.ParameterService;
+import org.kuali.rice.krad.service.BusinessObjectService;
+import org.kuali.rice.krad.uif.view.ViewModel;
+import org.kuali.coeus.propdev.impl.budget.core.ProposalBudgetForm;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -34,14 +42,59 @@ public class BudgetPersonnelCostElementValuesFinder extends CostElementValuesFin
 	@Autowired
     @Qualifier("parameterService")
     private ParameterService parameterService;
-	
+
+
+    @Autowired
+    @Qualifier("businessObjectService")
+    private BusinessObjectService businessObjectService;
+
     @Override
-    public List<KeyValue> getKeyValues() {
-        return super.getKeyValues(getPersonnelBudgetCategoryTypeCode(), true, null);
+    public List<KeyValue> getKeyValues(ViewModel model) {
+        String jobCode="";
+        BudgetPersonnelDetails details = ((ProposalBudgetForm)model).getAddProjectPersonnelHelper().getBudgetPersonnelDetail();
+        if (details != null && details.getBudgetPerson() != null) {
+            jobCode = ((ProposalBudgetForm) model).getAddProjectPersonnelHelper().getBudgetPersonnelDetail().getBudgetPerson().getJobCode();
+        }
+
+        return getValidCostElementForJobCode(jobCode);
     }
     
     private String getPersonnelBudgetCategoryTypeCode() {
         return this.getParameterService().getParameterValueAsString(Constants.MODULE_NAMESPACE_BUDGET, ParameterConstants.DOCUMENT_COMPONENT,Constants.BUDGET_CATEGORY_TYPE_PERSONNEL);
+    }
+
+
+    protected List<KeyValue> getValidCostElementForJobCode(String jobCode) {
+        List<KeyValue> allKeyValues  = super.getKeyValues(getPersonnelBudgetCategoryTypeCode(), true, null);
+        ArrayList<KeyValue> jobCodeLimitedKeyValues = new ArrayList<KeyValue>();
+
+        List<ValidCeJobCode> validCesFromJobCode=null;
+
+        Map<String, Object> fieldValues = new HashMap<String, Object>();
+        if(StringUtils.isNotEmpty(jobCode)) {
+            fieldValues.put("jobCode", jobCode.toUpperCase());
+            validCesFromJobCode = (List<ValidCeJobCode>) getBusinessObjectService().findMatching(ValidCeJobCode.class, fieldValues);
+
+            ArrayList<String> validCeCodes = new ArrayList<>();
+
+            if (validCesFromJobCode != null) {
+                for (ValidCeJobCode validCe : validCesFromJobCode) {
+                    validCeCodes.add(validCe.getCostElement());
+                }
+                Iterator<KeyValue> iter = allKeyValues.iterator();
+                while (iter.hasNext()) {
+                    KeyValue keyValue = iter.next();
+                    if (StringUtils.isEmpty(keyValue.getKey()) || validCeCodes.contains(keyValue.getKey())) {
+                        jobCodeLimitedKeyValues.add(keyValue);
+                    }
+                }
+            }
+        }
+
+        if (jobCodeLimitedKeyValues.size() > 1)  { allKeyValues= jobCodeLimitedKeyValues; }
+
+        return allKeyValues;
+
     }
 
 	public ParameterService getParameterService() {
@@ -51,4 +104,13 @@ public class BudgetPersonnelCostElementValuesFinder extends CostElementValuesFin
 	public void setParameterService(ParameterService parameterService) {
 		this.parameterService = parameterService;
 	}
+
+
+    public BusinessObjectService getBusinessObjectService() {
+        return businessObjectService;
+    }
+
+    public void setBusinessObjectService(BusinessObjectService businessObjectService) {
+        this.businessObjectService = businessObjectService;
+    }
 }
