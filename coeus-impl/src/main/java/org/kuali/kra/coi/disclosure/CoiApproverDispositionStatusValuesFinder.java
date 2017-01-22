9e1ff7ae110ebd73e335ef7727d42407aef668ad
Chris Denne <chris.denne@colostate.edu>
2017-01-22 11:40:34 -0700

    RSISSUES-91: part 2 fixing discl detail relatedness dropdown conflict.

--- coeus-impl/src/main/java/org/kuali/kra/coi/disclosure/CoiApproverDispositionStatusValuesFinder.java
+++ coeus-impl/src/main/java/org/kuali/kra/coi/disclosure/CoiApproverDispositionStatusValuesFinder.java
@@ -5,6 +5,7 @@ import org.kuali.kra.coi.CoiDisclosureDocument;
 import org.kuali.kra.coi.CoiDispositionStatus;
 import org.kuali.rice.core.api.util.ConcreteKeyValue;
 import org.kuali.rice.core.api.util.KeyValue;
+import org.kuali.rice.krad.util.GlobalVariables;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -19,25 +20,32 @@ public class CoiApproverDispositionStatusValuesFinder extends CoiDispositionStat
     @Override
     public List<KeyValue> getKeyValues() {
 
-        List<String> approverValues = new ArrayList<String> ();
-        approverValues.add("select");
-        approverValues.add("No Conflict Exists");
-        approverValues.add("A relevant management plan is already in place");
-        approverValues.add("A relevant mangement plan is already in place"); // <-- need to remove after maint cleanup
-        approverValues.add("A management plan should be developed and submitted for review");
 
         List<KeyValue> approverKeyValues = new ArrayList<KeyValue>();
         List<KeyValue> baseValues = super.getKeyValues();
+        List<String> approverValues = new ArrayList<String>();
+        CoiDisclosureDocument coiDisclosureDocument = (CoiDisclosureDocument) getDocument();
 
-        Iterator<KeyValue> valueIterator = baseValues.iterator();
+        if (coiDisclosureDocument.getDocumentHeader().getWorkflowDocument().isEnroute()) {
 
-        while (valueIterator.hasNext()) {
-            KeyValue kv = valueIterator.next();
-            if (approverValues.contains(kv.getValue())) {
-                approverKeyValues.add(kv);
+            approverValues.add("select");
+            approverValues.add("No Conflict Exists");
+            approverValues.add("A relevant management plan is already in place");
+            approverValues.add("A relevant mangement plan is already in place"); // <-- need to remove after maint cleanup
+            approverValues.add("A management plan should be developed and submitted for review");
+
+
+            Iterator<KeyValue> valueIterator = baseValues.iterator();
+
+            while (valueIterator.hasNext()) {
+                KeyValue kv = valueIterator.next();
+                if (approverValues.contains(kv.getValue())) {
+                    approverKeyValues.add(kv);
+                }
             }
+        } else {
+            approverKeyValues=baseValues;
         }
-
 
         return approverKeyValues;
     }
