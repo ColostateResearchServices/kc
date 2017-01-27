822e84b87efaa544ff35f3c91329be97df94348f
aakers <allen.akers@colostate.edu>
2017-01-27 15:58:15 -0700

    Added validations to insure that event date is entered as well as that the HR extension is valid before adding check type abbreviations on addition of a HireRight Done event.

--- coeus-impl/src/main/java/org/kuali/kra/excon/rules/ExconProjectEventAddRuleImpl.java
+++ coeus-impl/src/main/java/org/kuali/kra/excon/rules/ExconProjectEventAddRuleImpl.java
@@ -11,12 +11,14 @@ public class ExconProjectEventAddRuleImpl {
    
     private static final String EXCON_PROJECT_EVENT_ERROR_KEY = "exconProjectEventsBean.newEvent";
     private static final String ERROR_EXCON_PROJECT_EVENT_TYPE_IS_REQUIRED = "error.exconProjectEvent.eventTypeIsRequired";
+    private static final String ERROR_EXCON_PROJECT_EVENT_DATE_IS_REQUIRED = "error.exconProjectEvent.eventDateIsRequired";
     /**
      * @param event
      * @return
      */
     public boolean processAddExconProjectEventBusinessRules(ExconProject exconProject, ExconProjectEvent newEvent) {
         boolean valid = checkEventTypeIsValid(newEvent);
+        valid &= checkEventDateIsPresent(newEvent);
         return  valid;
     }
 
@@ -27,6 +29,14 @@ public class ExconProjectEventAddRuleImpl {
             return false;
         }
 
+        return true;
+    }
+
+    public boolean checkEventDateIsPresent(ExconProjectEvent newEvent) {
+        if (newEvent.getEventDate() == null) {
+            GlobalVariables.getMessageMap().putError(EXCON_PROJECT_EVENT_ERROR_KEY, ERROR_EXCON_PROJECT_EVENT_DATE_IS_REQUIRED);
+            return false;
+        }
         return true;
     }
 
