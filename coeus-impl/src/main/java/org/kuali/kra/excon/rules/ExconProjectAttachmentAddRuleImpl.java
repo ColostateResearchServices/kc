ce29e5416142cd81f21d054fd0d3c8cecb549971
aakers <allen.akers@colostate.edu>
2017-02-27 16:05:47 -0700

    RSISSUES-226: Added validation on adding a new attachment to verify that filename meets formatting requirements that will be applied on save, to avoid creating unusable projects.

--- coeus-impl/src/main/java/org/kuali/kra/excon/rules/ExconProjectAttachmentAddRuleImpl.java
+++ coeus-impl/src/main/java/org/kuali/kra/excon/rules/ExconProjectAttachmentAddRuleImpl.java
@@ -1,10 +1,18 @@
 package org.kuali.kra.excon.rules;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.struts.upload.FormFile;
 import org.kuali.coeus.common.framework.attachment.AttachmentFile;
 import org.kuali.kra.excon.project.ExconProject;
 import org.kuali.kra.excon.project.ExconProjectAttachment;
+import org.kuali.rice.krad.datadictionary.validation.constraint.AnyCharacterPatternConstraint;
 import org.kuali.rice.krad.util.GlobalVariables;
+
+import java.io.File;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 /**
  * This class implements the specified rule
@@ -14,6 +22,7 @@ public class ExconProjectAttachmentAddRuleImpl {
     private static final String EXCON_PROJECT_ATTACHMENT_ERROR_KEY = "exconProjectAttachmentsBean.newAttachment";
     private static final String ERROR_EXCON_PROJECT_ATTACHMENT_TYPE_IS_REQUIRED = "error.exconProjectAttachment.attachmentTypeIsRequired";
     private static final String ERROR_EXCON_PROJECT_ATTACHMENT_FILE_IS_REQUIRED = "error.exconProjectAttachment.attachmentFileIsRequired";
+    private static final String ERROR_ATTACHMENT_FILE_INVALID_FORMAT = "error.format.org.kuali.rice.kns.datadictionary.validation.charlevel.AnyCharacterValidationPattern.allowWhitespace";
 
     /**
      * @param event
@@ -36,11 +45,20 @@ public class ExconProjectAttachmentAddRuleImpl {
     }
     
     public boolean checkAttachmentFileIsValid(ExconProjectAttachment newAttachment) {
-        
-    	if (newAttachment.getNewFile() == null || StringUtils.isEmpty(AttachmentFile.createFromFormFile(newAttachment.getNewFile()).getName())) {
+        FormFile newFile=newAttachment.getNewFile();
+        String filename=AttachmentFile.createFromFormFile(newFile).getName();
+    	if (newFile == null || StringUtils.isEmpty(filename)) {
     		GlobalVariables.getMessageMap().putError(EXCON_PROJECT_ATTACHMENT_ERROR_KEY, ERROR_EXCON_PROJECT_ATTACHMENT_FILE_IS_REQUIRED);
             return false;
     	}
+
+        AnyCharacterPatternConstraint constraint = new AnyCharacterPatternConstraint();
+        constraint.setAllowWhitespace(true);
+
+        if (!filename.matches(constraint.getValue())) {
+            GlobalVariables.getMessageMap().putError(EXCON_PROJECT_ATTACHMENT_ERROR_KEY, ERROR_ATTACHMENT_FILE_INVALID_FORMAT, new String[]{"File Name ("+filename+")"});
+            return false;
+        }
     	return true;
     }
 
